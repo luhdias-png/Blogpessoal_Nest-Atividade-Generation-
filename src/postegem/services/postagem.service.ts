c0f6997cb90cfc432da1e0e2c58acdfd58ab80ba
luhdias-png <andre_lucas.94@hotmail.com>
2026-04-01 20:02:41 -0300

    Adicionado nova funcao chamado update!

--- src/postegem/services/postagem.service.ts
+++ src/postegem/services/postagem.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Postagem } from '../entities/postagem.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult } from 'typeorm/browser';
 
 @Injectable()
 export class PostagemService {
@@ -11,5 +12,37 @@ export class PostagemService {
   ) {}
   async findAll(): Promise<Postagem[]> {
     return await this.postegemRepository.find(); // select * from tb_postagem;
+  }
+
+  
+  async findBy(id: number): Promise<Postagem>{
+    
+  const postagem = await this.postegemRepository.findOne({
+    where: {
+      id
+    }});
+
+    if(!postagem)
+      throw new HttpException('Postagem não encontrada pq nao tem no bd sua mula', HttpStatus.NOT_FOUND);
+    
+    return postagem;
+  }
+
+  
+  async create(postagem: Postagem): Promise<Postagem>{
+    return await this.postegemRepository.save(postagem);
+  }
+  
+  async delete(id: number): Promise<DeleteResult>{
+    await this.findBy(id);
+    
+    return await this.postegemRepository.delete(id);
+  }
+  
+  async update(postagem: Postagem): Promise<Postagem>{
+
+    await this.findBy(postagem.id)
+
+    return await this.postegemRepository.save(postagem)
   }
 }
